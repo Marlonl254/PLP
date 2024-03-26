fc7ed99af6e50dbad08bf89b7e4e77729823e0ab
Marlonl254 <xmalcolm478@gmail.com>
2024-03-27 00:19:06 +0300

    User input

---
+++ Python/user_input.py
@@ -0,0 +1,8 @@
+'''
+Objective: To learn how to take user input in Python
+'''
+name = input("Enter your name: ")
+age  = input("Enter you age: ")
+location = input("Enter your location: ")
+
+print(f"Your is {name}, and you are {age} years old. You leave in {location}")
